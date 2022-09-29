fa65e549f015dbab0f9267d50fcc4d4d4b3ef0d2
Ilya Volkov <illvolkov@bk.ru>
2022-09-30 04:01:56 +0500

    Set CameraController as the root

--- QRScanner/Sources/App/AppDelegate.swift
+++ QRScanner/Sources/App/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = CameraController()
+        let cameraController = ModuleBuilder.buildCameraModule()
+        window.rootViewController = cameraController
         window.makeKeyAndVisible()
         self.window = window
         
