fb41923d2f4d24210bd498b9626a74c5835fef44
Ilya Volkov <illvolkov@bk.ru>
2022-09-29 19:57:30 +0500

    Initial Commit

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Ilya Volkov on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
